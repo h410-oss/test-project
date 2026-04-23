a9ad78a0de7f0fa972a560a447594e17e80f8c80
h410-oss <hironobu7924@gmail.com>
2026-04-23 16:13:46 +0900

    メニュー機能を追加

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 from profile import show_profile
 
-print("=== プログラム開始 ===")
-show_profile()
+while True:
+    print("=== メニュー ===")
+    print("1: プロフィール入力")
+    print("2: 終了")
+
+    choice = input("選択してください: ")
+
+    if choice == "1":
+        show_profile()
+    elif choice == "2":
+        print("終了します")
+        break
+    else:
+        print("もう一度選んでください")
